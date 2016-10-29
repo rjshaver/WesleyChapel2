559152965d493dc42bb0ec4fff662ecc97238849
Randy Shaver <rjshaver@windstream.net>
2016-10-29 03:02:00 -0400

    Working on getting all pages working.  Keep hitting bugs.  Home page fixed, content for sunday morning and directions done.  Need to find way to change content after loading for one section(main_content).

--- mobile/js/insert.js
+++ mobile/js/insert.js
@@ -26,4 +26,40 @@ $(document).ready(function(){
 }); 
 
 
+$(document).ready(function(){
+	document.getElementById("menu").getElementById("sundaymorning").onclick = function(){
+			alert("Sunady");
+			$.get("sundaymorninginsert.html", function(data){
+				$("#maincontent").html(data)});
+	
+	};
+
+	$(".home").click(function() {
+		alert("home");
+	$.get("home.html", function(data){
+		$("#maincontent").html(data);
+		});
+
+	});
+
+$(".sundaymorning").click(function() {
+	alert("Sunady");
+	$.get("sundaymorninginsert.html", function(data){
+		$("#maincontent").html(data);
+	});
+});
+
+$("#sundaymorning")
+
+$(".directions").click(function() {
+	alert("directions");
+	$.get("directions3.html", function(data){
+		$("#maincontent").html(data);
+	});
+});
+
+});
+
+
+
  

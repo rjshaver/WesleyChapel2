cd54832b2a380e7a013854bb22fbe9a44995a377
Randy Shaver <rjshaver@windstream.net>
2016-11-04 05:41:46 -0400

    Finally got all pages to display correctly.  Time to start cleaning up code and sorting out what can be deleted.

--- mobile/js/insert.js
+++ mobile/js/insert.js
@@ -1,36 +1,92 @@
-$(document).ready(function(){ 
-  $.get("imgs2.html", function(data) {
-    $("#images").html(data);
-  });
-}); 
+ var slideIndex = 0;
+ var x = document.getElementsByClassName("mySlides");
+
+function init_carousel() {
+    // var i;
+    // x = document.getElementsByClassName("mySlides");
+    // for (i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none";
+    // }
+    setTimeout(prime_carousel, 200);
+}
+
+function prime_carousel(){
+	 // var i;
+    x = document.getElementsByClassName("mySlides");
+    // for (i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none";
+    // }
+    // 
+    x[slideIndex ].style.display = "block";
+    setTimeout(carousel, 3800);
+
+}
+
+function carousel() {
+    // var i;
+    // var x = document.getElementsByClassName("mySlides");
+    // var x = document.getElementsByClassName("w3-animate-fading");
+    // for (i = 0; i < x.length; i++) {
+    //     x[i].style.display = "none";
+    // }
+    // x[slideIndex ].style.display = "none";
+    slideIndex++;
+    if (slideIndex > x.length-1) { slideIndex = 0 };
+    x[slideIndex ].style.display = "block";
+    if (slideIndex > 0) {
+    	x[slideIndex -1].style.display = "none";	
+    }
+    else {
+    	x[x.length -1].style.display = "none";	
+
+
+    }
+    
+    setTimeout(carousel, 4000); // Change image every 4 seconds
+}
 
 
 
 $(document).ready(function(){ 
-  $.get("menu.html", function(data) {
+  
+   $.get("menu.html", function(data) {
     $("#menu").html(data);
   });
-}); 
-
-
-$(document).ready(function(){ 
-  $.get("footer.html", function(data) {
-    $("#footr").html(data);
+   $.get("imgs2.html", function(data) {
+    $("#images").html(data);
   });
-}); 
-
-$(document).ready(function(){ 
-  $.get("home.html", function(data) {
+   $.get("home.html", function(data) {
     $("#maincontent").html(data);
   });
-}); 
-
-
-$(document).ready(function(){ 
-  $.get("facebook.html", function(data) {
-    $("#afacehtml").html(data);
+    $.get("footer.html", function(data) {
+    $("#footr").html(data);
   });
+  init_carousel();
 }); 
+
+
+
+// $(document).ready(function(){ 
+//   $.get("menu.html", function(data) {
+//     $("#menu").html(data);
+//   });
+// }); 
+
+
+// $(document).ready(function(){ 
+//   $.get("footer.html", function(data) {
+//     $("#footr").html(data);
+//   });
+// }); 
+
+// $(document).ready(function(){ 
+//   $.get("home.html", function(data) {
+//     $("#maincontent").html(data);
+//   });
+// }); 
+
+
+ 
 
 
 
@@ -40,6 +96,7 @@ function home(){
 	  $.get("home.html", function(data) {
     $("#maincontent").html(data);
   });
+
 }
 
 function sundaymorning(){
